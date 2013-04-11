721aec66fab98d0a5c2703b07214bc5ba35f7f63
nelsonsozinho <nelsonsozinho@gmail.com>
2013-04-11 16:11:11 -0400

    finish the update implementation

--- app/assets/javascripts/search_jquery.js
+++ app/assets/javascripts/search_jquery.js
@@ -1,6 +1,4 @@
 $('#search_bn').click(function() {
-	//console.log('Respondeu');
-	//console.log($("#term").val());
 	var url = "/search/search_ramal"
 	$.ajax({
 		type: "POST",
@@ -12,8 +10,6 @@ $('#search_bn').click(function() {
 });
 
 $('#search_bn_edit').click(function() {
-	//console.log('Respondeu');
-	//console.log($("#term").val());
 	var url = "/search/search_ramal"
 	$.ajax({
 		type: "POST",
@@ -41,17 +37,16 @@ $.build_result_edit = function(json) {
 	$.each(json, function(index,item) {
 		$('#id_body_edit_result').append("<tr id=id_tr_" + item.id  + ">");
 		$('#id_body_edit_result').append("<td>#</td>");
-		$('#id_body_edit_result').append("<td>" + item.name + "</td>");
-		$('#id_body_edit_result').append("<td>" + item.number + "</td>");
-		$('#id_body_edit_result').append("<td>" + item.local + "</td>");
-		$('#id_body_edit_result').append("<td> <input type='button' class='btn btn-info' id='" + item.id + "' ></input> </td>");
+		$('#id_body_edit_result').append("<td class='fields' name='field_name'>" + item.name + "</td>");
+		$('#id_body_edit_result').append("<td class='fields' name='field_number'>" + item.number + "</td>");
+		$('#id_body_edit_result').append("<td class='fields' name='field_local'>" + item.local + "</td>");
+		$('#id_body_edit_result').append("<td> <input type='button' class='btn btn-info' id='" + item.id + "' ></input> <input type='button' class='btn btn-danger' id='" + item.id + "' ></input> </td>");
 		$('#id_body_edit_result').append("</tr>");
 	});
 };
 
 $(document).on("click", ".btn-info", function() {
 	var url_request = "/ramal/requestramal"
-	$("#id_div_editmodal").modal();	
 	$.ajax({
         url: url_request, 
         data: {id:$(this).attr("id")},
@@ -59,12 +54,26 @@ $(document).on("click", ".btn-info", function() {
         type: "GET"
     }).success(function(json){
     	console.log(json);
-        $("#id_input_name").attr("value",json.name);
-        $("#id_input_local").attr("value",json.local);
-        $("#id_input_number").attr("value",json.number);
-        $("#id_input_id").attr("value",json.id);
+        $("#id_input_name").val(json.name);
+        $("#id_input_local").val(json.local);
+        $("#id_input_number").val(json.number);
+        $("#id_input_id").val(json.id);
+        $("#id_div_editmodal").modal();	
     });
 });
+
+$(document).on("click", ".btn-danger", function() {
+	var url_request = "/ramal/deleteramal"
+	$.ajax({
+        url: url_request, 
+        data: {id:$(this).attr("id")},
+        dataType: "JSON", 
+        type: "DELETE"
+    }).success(function(json){
+    	console.log(json);
+        $("#id_div_editmodal").modal();	
+    });
+}
 
 $("#id_button_submit").click(function() {
 	var url_update = "/ramal/update";
@@ -73,14 +82,28 @@ $("#id_button_submit").click(function() {
 	console.log(elements);
 	$.ajax({
         url: url_update,
-        //data: {name: $("id_input_name").val(), number: $("id_input_number").val(), local: $("id_input_local").val()},
         data: elements,
         dataType: "JSON", 
         type: "POST"
     }).success(function(json){
-        console.log("Requisicao enviada com sucesso");
         var id = json.id;
+        $.limpar_campos_modal();
         
+        $.ajax({
+			type: "POST",
+			url: "/search/search_ramal", 
+			data: {busca: $("#term").val()}
+		}).done(function(json) {
+			$.build_result_edit(json);
+		});
 
+        $("#id_div_editmodal").modal("hide");
     });
 });
+
+$.limpar_campos_modal = function() {
+	$("#id_input_name").val("");
+    $("#id_input_local").val("");
+    $("#id_input_number").val("");
+    $("#id_input_id").val("");
+};
